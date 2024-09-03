0126a59f67acfcf126153c27bb9272cba65c090e
Danilo Hoffmann <dhhyi@aol.com>
2024-09-03 10:42:12 +0000

    use pretty tutorial rules

--- src/app/rules/rules.persistence.ts
+++ src/app/rules/rules.persistence.ts
@@ -4,9 +4,22 @@ import {
   saveLocalStorage,
 } from '../../util/localstorage.persistence';
 
+const template = `:- Do you want to find out how this app works? $explore(false);
+
+explore :-
+[Tutorial] This app creates a conditional checklist for you after you answer questions.,
+Want to know even more? $more(false);
+
+explore AND more :-
+[Tutorial] You will find documentation in the configuration :);
+
+explore OR more :-
+[Tutorial] Have fun exploring everything.;
+`;
+
 @Injectable({ providedIn: 'root' })
 export class RulesPersistence {
-  private rules: string = loadLocalStorage('rules', ':- [category] Item;');
+  private rules: string = loadLocalStorage('rules', template);
 
   getRules(): string {
     return this.rules;
