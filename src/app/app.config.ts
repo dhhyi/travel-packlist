612309ef308485d7278eb652c0867667fad836e8
Danilo Hoffmann <dhhyi@aol.com>
2024-09-05 08:11:25 +0000

    add service-worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withHashLocation()),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
