1bbcb77937461095d0ab8ffcfa45fa60d46c0116
Danilo Hoffmann <dhhyi@aol.com>
2024-09-10 11:19:22 +0000

    type support for jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/src/setup-jest.ts"],
 };
+
+module.exports = config;
