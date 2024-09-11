619fb60a451cecccabf818bc4887d437d7302bee
Danilo Hoffmann <dhhyi@aol.com>
2024-09-11 11:56:54 +0000

    cleanup

--- convert-documentation.mjs
+++ convert-documentation.mjs
@@ -2,8 +2,12 @@ import showdown from "showdown";
 import fs from "fs";
 
 const converter = new showdown.Converter();
-const markdown = fs.readFileSync("src/model/documentation.md", "utf8");
+
+const markdown = fs.readFileSync("src/model/documentation.md", {
+  encoding: "utf-8",
+});
 const html = converter.makeHtml(markdown);
+
 fs.writeFileSync(
   "src/app/rules/documentation/documentation.component.html",
   html,
