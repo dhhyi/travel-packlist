a82399ede334e2e0b7c1a06d0a5534eed5649c4b
Danilo Hoffmann <dhhyi@aol.com>
2024-09-20 16:31:16 +0000

    change template

--- src/model/template.ts
+++ src/model/template.ts
@@ -6,11 +6,14 @@ export const rulesTemplate = `# This is the rules template
    [Utility] Backpack;
 
 sunny :-
+   Do you expect a high UV index? $uv,
    [Utility] Sunglasses,
-   [Utility] Sunscreen,
    [Clothes] Short Pants;
 
 NOT sunny :-
    [Clothes] Jacket,
    [Clothes] Long Pants;
+
+uv :-
+   [Utility] Sunscreen;
 `;
