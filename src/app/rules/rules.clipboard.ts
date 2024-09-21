dadcea9c84fa3b56c55e59d94b4b25de60311452
Danilo Hoffmann <dhhyi@aol.com>
2024-09-21 12:20:50 +0000

    use signals for clipboard for OnPush to work properly

--- src/app/rules/rules.clipboard.ts
+++ src/app/rules/rules.clipboard.ts
@@ -1,31 +1,27 @@
-import { Injectable } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Item, Question } from '../../model/types';
 
 @Injectable({ providedIn: 'root' })
 export class RulesClipboard {
-  private questions: Question[] = [];
-  private items: Item[] = [];
+  private questions = signal<Question[]>([]);
+  private items = signal<Item[]>([]);
 
   cutQuestion(question: Question) {
-    this.questions.push(question);
+    this.questions.update((questions) => [...questions, question]);
   }
 
   cutItem(item: Item) {
-    this.items.push(item);
+    this.items.update((items) => [...items, item]);
   }
 
-  get itemsCount(): number {
-    return this.items.length;
-  }
+  itemsCount = computed(() => this.items().length);
 
-  get questionsCount(): number {
-    return this.questions.length;
-  }
+  questionsCount = computed(() => this.questions().length);
 
   paste(): { questions: Question[]; items: Item[] } {
-    const clipboard = { questions: this.questions, items: this.items };
-    this.questions = [];
-    this.items = [];
+    const clipboard = { questions: this.questions(), items: this.items() };
+    this.questions.set([]);
+    this.items.set([]);
     return clipboard;
   }
 }
