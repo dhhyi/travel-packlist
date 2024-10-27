ab42042e5a0e95e13e37af39376f921da7077cbd
Danilo Hoffmann <dhhyi@aol.com>
2024-10-27 13:12:30 +0000

    converter for legacy state chunks

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -39,10 +39,54 @@ export class AppState {
     const loaded = localStorage.getItem('state') ?? '{}';
     this.state = JSON.parse(loaded) as State;
     this.state = { ...initialState, ...this.state };
+    this.loadLegacyState();
     // initialize signals
     for (const key of Object.keys(initialState) as Keys[]) {
       this.signal(key);
     }
+  }
+
+  /**
+   * Load legacy state from localStorage.
+   * This is only necessary for users who have used the old version of the checklist.
+   *
+   * TODO: Remove this function after the next major release.
+   */
+  private loadLegacyState() {
+    /* eslint-disable @typescript-eslint/no-non-null-assertion */
+    if (localStorage.getItem('answers')) {
+      this.state.answers = JSON.parse(
+        localStorage.getItem('answers')!,
+      ) as State['answers'];
+      localStorage.removeItem('answers');
+    }
+    if (localStorage.getItem('checkedItems')) {
+      this.state.checkedItems = JSON.parse(
+        localStorage.getItem('checkedItems')!,
+      ) as State['checkedItems'];
+      localStorage.removeItem('checkedItems');
+    }
+    if (localStorage.getItem('collapsedCategories')) {
+      this.state.collapsedCategories = JSON.parse(
+        localStorage.getItem('collapsedCategories')!,
+      ) as State['collapsedCategories'];
+      localStorage.removeItem('collapsedCategories');
+    }
+    if (localStorage.getItem('rules')) {
+      this.state.rules = localStorage.getItem('rules')!;
+      localStorage.removeItem('rules');
+    }
+    if (localStorage.getItem('config')) {
+      const config = JSON.parse(localStorage.getItem('config')!) as State;
+      this.state.fadeOutDisabledRules = config.fadeOutDisabledRules;
+      this.state.trackWeight = config.trackWeight;
+      this.state.answersLocked = config.answersLocked;
+      this.state.theme = config.theme;
+      this.state.language = config.language;
+      localStorage.removeItem('config');
+    }
+    /* eslint-enable @typescript-eslint/no-non-null-assertion */
+    this.persist();
   }
 
   private persist() {
