ebe66ff0ef8c44869db315e00e16f7f4f8551365
Danilo Hoffmann <dhhyi@aol.com>
2024-11-05 13:42:06 +0000

    extend rules template

--- src/app/model/template.ts
+++ src/app/model/template.ts
@@ -1,9 +1,30 @@
-export const rulesTemplate = `# This is the rules template
+export const rulesTemplate = `:-
+   [Utility] Paper Tissues,
+   [Utility] Backpack;
+
+:- Are you traveling longer than 3 days? $longer;
+
+longer :-
+   Do you have a washer or laundry service available? $laundry,
+   [Electronics] Phone Charger,
+   [Electronics] Tablet,
+   Do you want to work? $work;
+
+longer AND NOT laundry :-
+   [Clothes] enough Shirts,
+   [Clothes] enough Underwear,
+   [Clothes] spare Pants;
+
+laundry :-
+   [Clothes] 3 Shirts,
+   [Clothes] 3 sets of Underwear;
+
+work :-
+   [Electronics] Laptop;
 
 :-
    Will it be sunny? $sunny,
-   [Utility] Paper Tissues,
-   [Utility] Backpack;
+   Are you traveling abroad? $abroad;
 
 sunny :-
    Do you expect a high UV index? $uv,
@@ -16,4 +37,8 @@ NOT sunny :-
 
 uv :-
    [Utility] Sunscreen;
+
+abroad :-
+   [Documents] Passport,
+   [Documents] Visa;
 `;
