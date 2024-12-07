781bbb55a6ab7fbffd94dd8821f38bc933b4cb6d
Danilo Hoffmann <dhhyi@aol.com>
2024-12-07 14:59:28 +0000

    make pre-commit script runnable

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ module.exports = tseslint.config(
   },
   {
     files: ["**/*.mjs"],
+    ignores: ["src/app/generated/**"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
