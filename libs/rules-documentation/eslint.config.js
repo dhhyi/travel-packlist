3b57f9a8953eeed739857888db52f50ac621fc3e
Danilo Hoffmann <dhhyi@aol.com>
2024-12-19 20:35:20 +0000

    use generator for generating rules documentation

--- libs/rules-documentation/eslint.config.js
+++ libs/rules-documentation/eslint.config.js
@@ -1,5 +1,10 @@
+const { includeIgnoreFile } = require('@eslint/compat');
 const nx = require('@nx/eslint-plugin');
 
 const baseConfig = require('../../eslint.config.js');
 
-module.exports = [...baseConfig, ...nx.configs['flat/angular']];
+module.exports = [
+  includeIgnoreFile(require.resolve('./.gitignore')),
+  ...baseConfig,
+  ...nx.configs['flat/angular'],
+];
---
+++ tools/generate-plugin/src/generators/documentation/generator.ts
@@ -0,0 +1,41 @@
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import { globSync } from 'glob';
+import * as path from 'path';
+import * as showdown from 'showdown';
+
+import { GeneratorSchema } from './schema';
+
+export async function generator(tree: Tree, options: GeneratorSchema) {
+  const files = globSync(options.pattern);
+
+  const languages = files.map((file) => {
+    const name = path.basename(file, path.extname(file));
+    const lang = path.extname(name).substring(1) || 'default';
+
+    const converter = new showdown.Converter();
+    const markdown = tree.read(file).toString('utf-8');
+    const html = converter.makeHtml(markdown);
+
+    const folder = path.join(options.path);
+
+    generateFiles(
+      tree,
+      path.join(__dirname, 'files', 'documentation'),
+      folder,
+      {
+        lang,
+        html,
+      },
+    );
+
+    return lang;
+  });
+
+  generateFiles(tree, path.join(__dirname, 'files', 'src'), options.path, {
+    languages,
+  });
+
+  await formatFiles(tree);
+}
+
+export default generator;
