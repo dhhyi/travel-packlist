57ed21b38007d8488dadde2825e1429419befad0
Danilo Hoffmann <dhhyi@aol.com>
2024-12-19 13:51:17 +0000

    convert locale check to executor

---
+++ tools/check-plugin/src/executors/translations/executor.ts
@@ -0,0 +1,91 @@
+import { PromiseExecutor } from '@nx/devkit';
+import { readFileSync, writeFileSync } from 'fs';
+import { globSync } from 'glob';
+
+import { ExecutorSchema } from './schema';
+
+interface Translations {
+  locale: string;
+  translations: Record<string, string>;
+}
+
+const run: PromiseExecutor<ExecutorSchema> = async (options) => {
+  try {
+    let errors = false;
+    const files = globSync(options.pattern);
+
+    if (files.length === 0) {
+      throw new Error(`No files found for pattern: ${options.pattern}`);
+    }
+
+    const translations = files.map((file) => {
+      const content = readFileSync(file, { encoding: 'utf-8' });
+
+      return { ...(JSON.parse(content) as Translations), file };
+    });
+
+    translations.forEach((translation) => {
+      Object.keys(translation.translations).forEach((key) => {
+        if (!/^[a-z][a-z0-9.-]*[a-z0-9]$/.test(key)) {
+          console.warn(`Key "${key}" is not snake case`);
+          errors = true;
+        }
+      });
+    });
+
+    translations.forEach((translation) => {
+      translation.translations = Object.fromEntries(
+        Object.entries(translation.translations)
+          .map(([key, value]) => [key.trim(), value.trim()] as const)
+          .sort(([k1], [k2]) => k1.localeCompare(k2)),
+      );
+    });
+
+    const defaultLocale = translations.find(
+      (t) => t.locale === options.defaultLocale,
+    );
+
+    if (!defaultLocale) {
+      throw new Error(`Default locale not found: ${options.defaultLocale}`);
+    }
+
+    const defaultKeys = Object.keys(defaultLocale.translations);
+
+    translations
+      .filter((t) => t.locale !== options.defaultLocale)
+      .map((translation) => {
+        translation.translations = Object.fromEntries(
+          Object.entries(translation.translations).filter(([key]) => {
+            if (!defaultKeys.includes(key)) {
+              console.warn(`Key "${key}" not found in default locale`);
+              errors = true;
+              return false;
+            }
+
+            return true;
+          }),
+        );
+      });
+
+    translations.forEach((translation) => {
+      writeFileSync(
+        translation.file,
+        JSON.stringify(
+          {
+            locale: translation.locale,
+            translations: translation.translations,
+          },
+          null,
+          2,
+        ) + '\n',
+      );
+    });
+
+    return { success: !errors };
+  } catch (error) {
+    console.error(error.message);
+    return { success: false };
+  }
+};
+
+export default run;
