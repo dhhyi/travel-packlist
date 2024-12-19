7fe374612bfed486b2cb8d5e1600658743b198c1
Danilo Hoffmann <dhhyi@aol.com>
2024-12-19 17:39:07 +0000

    convert json2ts invocations to implicit executor

---
+++ tools/json-schema-to-typescript/src/index.ts
@@ -0,0 +1,56 @@
+import { createNodesFromFiles, CreateNodesV2 } from '@nx/devkit';
+import { createHash } from 'crypto';
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+export const createNodesV2: CreateNodesV2<object> = [
+  '**/schema.json',
+  async (schemaFiles, options, context) => {
+    return await createNodesFromFiles(
+      (schemaFile) => createNodesInternal(schemaFile),
+      schemaFiles,
+      options,
+      context,
+    );
+  },
+];
+
+async function createNodesInternal(schemaInput: string) {
+  const project = schemaInput.replace(/\/src\/.*$/, '');
+  const schemaInputRelative = schemaInput.replace(project + '/', '');
+
+  const isProject =
+    existsSync(join(project, 'project.json')) ||
+    existsSync(join(project, 'package.json'));
+  if (!isProject) {
+    return {};
+  }
+
+  const schemaInputWithRoot = `{projectRoot}/${schemaInputRelative}`;
+  const schemaOutputWithRoot = schemaInputWithRoot.replace(/\.json$/, '.d.ts');
+
+  const shaPath = createHash('sha1')
+    .update(schemaInput)
+    .digest('hex')
+    .substring(0, 8);
+
+  return {
+    projects: {
+      [project]: {
+        targets: {
+          ['schema2ts-' + shaPath]: {
+            executor: 'nx:run-commands',
+            options: {
+              commands: [
+                `json2ts --input=${schemaInputWithRoot} --output=${schemaOutputWithRoot}`,
+              ],
+            },
+            inputs: [schemaInputWithRoot],
+            outputs: [schemaOutputWithRoot],
+            cache: true,
+          },
+        },
+      },
+    },
+  };
+}
