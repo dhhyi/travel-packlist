de9d59b482a9eb9fcc533cee91284fd91aa98330
dhhyi <dhhyi@aol.com>
2025-01-28 14:02:41 +0000

    Deploying to gh-pages from @ dhhyi/travel-packlist@f2b42dd2302da49781adf8635c54f3b48a449eb2 🚀

---
+++ chunk-16a8e83d.js
@@ -0,0 +1,2 @@
+import{s}from"./chunk-d304b66f.js";import{ea as t,zc as e}from"./chunk-5879788f.js";var r=class{state=t(s);exportFileName=e(()=>{let a=new Date(this.state.rules.lastAction()).toISOString().replace(/\..*$/,"").replace(/[T:]/g,"-"),o=this.state.rules.hash();return`travel-packlist-rules-${a}-${o}.txt`})};export{r as a};
+/**i18n:5366b86f2516668e0182d7169675f5a9db84e9239a33e605b62f0d042910cdf2*/
