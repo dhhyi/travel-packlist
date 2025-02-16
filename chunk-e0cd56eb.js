52ee850fc17000d170e431f334d600f18d078bee
dhhyi <dhhyi@aol.com>
2025-02-16 13:05:44 +0000

    Deploying to gh-pages from @ dhhyi/travel-packlist@22e2f31c631e3f0fca38e9500bfd04bd3ab78b71 🚀

---
+++ chunk-e0cd56eb.js
@@ -0,0 +1,2 @@
+import{b as e}from"./chunk-94035e8a.js";import"./chunk-243a5d09.js";var r=class extends e{async canShare(){return typeof navigator>"u"||!navigator.share?{value:!1}:{value:!0}}async share(a){if(typeof navigator>"u"||!navigator.share)throw this.unavailable("Share API not available in this browser");return await navigator.share({title:a.title,text:a.text,url:a.url}),{}}};export{r as ShareWeb};
+/**i18n:15693acdce30f00c919aab2f8515f765f213a298081817f04f3b278c0f34be30*/
