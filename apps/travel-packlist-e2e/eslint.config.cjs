90846083dacb05e5c4b82004fc7f9deaa636f004
Danilo Hoffmann <dhhyi@aol.com>
2025-04-20 09:50:58 +0000

    ignore e2e test report

--- apps/travel-packlist-e2e/eslint.config.cjs
+++ apps/travel-packlist-e2e/eslint.config.cjs
@@ -1,9 +1,12 @@
+const { includeIgnoreFile } = require('@eslint/compat');
+
 const playwright = require('eslint-plugin-playwright');
 
 const baseConfig = require('../../eslint.base.config.cjs');
 const typescriptRules = require('../../util/typescript-rules.js');
 
 module.exports = [
+  includeIgnoreFile(require.resolve('./.gitignore')),
   playwright.configs['flat/recommended'],
   ...baseConfig,
   ...typescriptRules(__dirname),
