fa756739228fff0f9df0a6f5382e6ea9d8c15a35
Danilo Hoffmann <dhhyi@aol.com>
2025-04-22 15:12:44 +0000

    extend items for weights e2e test

--- apps/travel-packlist-e2e/src/weights.spec.ts
+++ apps/travel-packlist-e2e/src/weights.spec.ts
@@ -4,15 +4,16 @@ import { startWithRules } from './pages';
 
 const rulesWithWeights = `:- [tool] umbrella 150, [tool] backpack 1kg;
     :- Will it rain? $rainy;
-    :- [clothes] raincoat 500g, [clothes] t-shirt 200g, [clothes] shorts 300g;
-    :- [food] sandwich 200g, [food] apple 100g, [food] water 590g;`;
+    :- [clothes] jacket 350g, [clothes] t-shirt 200g, [clothes] shorts 200g;
+    :- [food] sandwich 200g, [food] apple 100g, [food] water 1000g;
+    :- [electronics] phone 200g, [electronics] charger 100g, [electronics] power bank 500g;`;
 
 test('weight tracking', async ({ page }) => {
   const packlist = await startWithRules(page, rulesWithWeights);
 
   await expect(packlist.weightPackingProgress()).toBeHidden();
   await expect(packlist.itemPackingProgress()).toMatchAriaSnapshot(`
-    - progressbar "You have packed 0 out of 8 items."
+    - progressbar "You have packed 0 out of 11 items."
   `);
 
   const config = await packlist.toConfigPage();
@@ -26,13 +27,13 @@ test('weight tracking', async ({ page }) => {
 
   await expect(packlist.itemPackingProgress()).toBeHidden();
   await expect(packlist.weightPackingProgress()).toMatchAriaSnapshot(`
-    - progressbar "You have packed 0g out of 3.0kg by packing 0 out of 8 items."
+    - progressbar "You have packed 0g out of 4.0kg by packing 0 out of 11 items."
   `);
 
   await packlist.item('umbrella', false).click();
 
   await expect(packlist.weightPackingProgress()).toMatchAriaSnapshot(`
-    - progressbar "You have packed 150g out of 3.0kg by packing 1 out of 8 items."
+    - progressbar "You have packed 150g out of 4.0kg by packing 1 out of 11 items."
   `);
   await expect(page).toHaveScreenshot();
 
@@ -40,13 +41,13 @@ test('weight tracking', async ({ page }) => {
 
   await packlist.displayWeightDistributionButton().click();
 
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 
   await expect(packlist.displayHeaviestItemsButton()).toBeVisible();
 
   await packlist.displayHeaviestItemsButton().click();
 
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
 
 test('weight tracking activation', async ({ page }) => {
