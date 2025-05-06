e1ae63ebb98c0ec60327320f99d4aa9cc575e0dc
Danilo Hoffmann <dhhyi@aol.com>
2025-05-06 19:43:13 +0000

    switches for accessibility and animations in design-system

--- apps/design-system/storybook/preview.ts
+++ apps/design-system/storybook/preview.ts
@@ -2,6 +2,8 @@ import { withThemeByClassName } from '@storybook/addon-themes';
 import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { type Preview, componentWrapperDecorator } from '@storybook/angular';
 
+// https://storybook.js.org/docs/faq#what-icons-are-available-for-my-toolbar-or-my-addon
+
 const preview: Preview = {
   parameters: {
     viewport: {
@@ -9,12 +11,46 @@ const preview: Preview = {
       defaultViewport: 'mobile2',
     },
   },
+  globalTypes: {
+    accessible: {
+      description: 'Accessibility',
+      toolbar: {
+        title: 'Accessible',
+        icon: 'accessibility',
+        items: [
+          { value: 'true', right: true, title: '✔️' },
+          { value: 'false', title: '❌' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+    animations: {
+      description: 'Animations',
+      toolbar: {
+        title: 'Animations',
+        icon: 'fastforward',
+        items: [
+          { value: 'true', right: true, title: '✔️' },
+          { value: 'false', title: '❌' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+  initialGlobals: {
+    accessible: 'true',
+    animations: 'true',
+  },
   decorators: [
     componentWrapperDecorator(
       (story) => `
-        <div class="container mx-auto" style="padding-bottom: 4rem;">
+        <div [class.accessible]="accessible" [class.animations]="animations" class="container mx-auto" style="padding-bottom: 4rem;">
           ${story}
         </div>`,
+      ({ globals }) => ({
+        accessible: globals['accessible'] === 'true',
+        animations: globals['animations'] === 'true',
+      }),
     ),
     withThemeByClassName({
       themes: {
