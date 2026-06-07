77e653734cc4a6cefe39e9289f69f67dfff77699
Danilo Hoffmann <dhhyi@aol.com>
2026-06-07 14:01:41 +0000

    fix: re-check error status on error page

--- apps/travel-packlist/src/app/pages/rules-error/rules-error.component.ts
+++ apps/travel-packlist/src/app/pages/rules-error/rules-error.component.ts
@@ -2,8 +2,10 @@ import {
   ChangeDetectionStrategy,
   Component,
   computed,
+  effect,
   inject,
 } from '@angular/core';
+import { Router } from '@angular/router';
 import { GLOBAL_STATE } from '@travel-packlist/state';
 
 import { extractErrorMessage } from '../../util/extract-error-message';
@@ -16,20 +18,42 @@ import { extractErrorMessage } from '../../util/extract-error-message';
 export class RulesErrorComponent {
   private state = inject(GLOBAL_STATE);
 
-  readonly error = computed<{ type: 'parsing' | 'fetching'; message: unknown }>(
-    () =>
-      this.state.rules.raw.status() === 'error'
+  readonly error = computed(() =>
+    this.state.rules.raw.status() === 'error'
+      ? {
+          type: 'fetching' as const,
+          error: this.state.rules.raw.error(),
+        }
+      : this.state.rules.parsed.status() === 'error'
         ? {
-            type: 'fetching',
-            message: extractErrorMessage(this.state.rules.raw.error()),
+            type: 'parsing' as const,
+            error: this.state.rules.parsed.error(),
           }
-        : {
-            type: 'parsing',
-            message: extractErrorMessage(this.state.rules.parsed.error()),
-          },
+        : { type: 'none' as const },
   );
 
+  readonly errorText = computed(() => {
+    const error = this.error();
+    if (error.type !== 'none') {
+      return extractErrorMessage(error.error);
+    } else {
+      return undefined;
+    }
+  });
+
   readonly rulesMode = this.state.rules.mode;
+
+  constructor() {
+    const router = inject(Router);
+    effect(() => {
+      if (
+        !this.state.router.navigationRunning() &&
+        this.error().type === 'none'
+      ) {
+        void router.navigateByUrl(location.hash.substring(1));
+      }
+    });
+  }
 
   goToRawEditor() {
     this.state.router.go('rules-raw');
